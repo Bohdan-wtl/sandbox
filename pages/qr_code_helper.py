5272611bbbb33b6664fcd0b474169d8cf43dfe62
Bohdan-wtl <bohdan.chornyi@white-test.com>
2024-11-04 13:23:35 +0200

    helper

--- pages/qr_code_helper.py
+++ pages/qr_code_helper.py
@@ -8,6 +8,7 @@ from faker import Faker
 from pydub.generators import Sine
 from moviepy.editor import ColorClip
 
+
 class QrCodeHelper:
     def __init__(self, page, locator):
         self.page = page
@@ -24,10 +25,10 @@ class QrCodeHelper:
         if file_type == 'image':
             image_filename = f"generated_image_{timestamp}.png"
             image_path = file_path / image_filename
-            image = Image.new('RGB', (800, 600), color=(0, 111, 255))
+            image = Image.new('RGB', (800, 600), color=(000, 111, 255))
             draw = ImageDraw.Draw(image)
             font = ImageFont.load_default()
-            text = f"{self.faker.text(max_nb_chars=500)} {timestamp}"
+            text = f"{self.faker.texts(max_nb_chars=500)} {timestamp}"
             draw.text((10, 10), text, font=font, fill=(0, 0, 0))
             image.save(image_path)
             return image_path
@@ -50,8 +51,8 @@ class QrCodeHelper:
             pdf = FPDF()
             pdf.add_page()
             pdf.set_font("Arial", size=12)
-            pdf.multi_cell(190, 10, txt=f"{self.faker.text(max_nb_chars=5000)} {timestamp}", align='C')
-            pdf.output(str(pdf_path))
+            pdf.multi_cell(190, 10, txt=f"{self.faker.texts(max_nb_chars=5000)} {timestamp}", align='C')
+            pdf.output(pdf_path)
             return pdf_path
 
         elif file_type == 'mp3':
@@ -95,43 +96,21 @@ class QrCodeHelper:
             'mimeType': mime_type,
             'fileName': file_path.name
         })
-        self.page.wait_for_selector("//span[@class='image-edit-icon']", state="visible")
 
     def set_file(self, selector, file_type):
         file_path = self.generate_file(file_type)
-        self.page.locator(selector).set_input_files(file_path)
+        selector.set_input_files(file_path)
+
 
     def close_help_modal_window_st3(self):
-        if self.locator.help_modal_close_button.is_visible():
-            self.locator.help_modal_close_button.click()
-        else:
-            pass
+        self.locator.help_modal_close_button.is_visible()
+        self.locator.help_modal_close_button.click()
 
     def close_help_modal_window_st2(self):
-        if self.locator.help_modal_close_button.is_visible():
-            self.locator.help_modal_close_button.click()
-            self.page.wait_for_selector(self.locator.modal_window_step2, state="hidden", timeout=5000)
-        else:
-            pass
-
-    def wait_for_loader_disappear(self):
-        if self.locator.loader_img.is_visible():
-            self.page.wait_for_selector(self.locator.loader_img, state="hidden")
-        else:
-            pass
-
-    def select_random_colors(self):
-        design_color_style_locators = [
-            "//div[@id='formcolorPalette1']",
-            "//div[@id='formcolorPalette2']",
-            "//div[@id='formcolorPalette4']",
-            "//div[@id='formcolorPalette5']",
-            "//div[@id='formcolorPalette6']",
-            "//div[@id='formcolorPalette7']"
-        ]
-        random_design = random.choice(design_color_style_locators)
-        self.page.locator(random_design).click()
-        return random_design
+        self.locator.help_modal_close_button.wait_for(state="visible")
+        self.locator.help_modal_close_button.is_enabled()
+        self.locator.help_modal_close_button.click()
+        self.page.wait_for_selector(self.locator.modal_window_step2, state="hidden", timeout=5000)
 
     def set_custom_qr_code_name(self, qr_code_type):
         self.locator.custom_name_qr_code_dropdown.click()
@@ -158,7 +137,7 @@ class QrCodeHelper:
     def welcome_screen_set_img(self):
         image_path = self.generate_file('image')
         self.locator.upload_welcome_screen_qr_code_dropdown.click()
-        self.page.locator(self.locator.upload_welcome_screen_qr_code_input).set_input_files(image_path)
+        self.locator.upload_welcome_screen_qr_code_input.set_input_files(image_path)
 
     def select_random_option(self, locators):
         random_locator = random.choice(locators)
@@ -166,21 +145,25 @@ class QrCodeHelper:
         return random_locator
 
     def select_frame_step3(self):
+        self.locator.frame_step3_dropdown.is_visible()
+        self.locator.frame_step3_dropdown.is_enabled()
         self.locator.frame_step3_dropdown.click()
         frame_locators = [
             "//button[@id='qr_frame_id_{}']".format(i) for i in range(16)
         ]
         return self.select_random_option(frame_locators)
 
-    def select_pattern_step3(self):
-        self.locator.qrcode_patterns_step3_dropdown.click()
-        pattern_locators = [
-            "//label[@id='square']", "//label[@id='round']", "//label[@id='extra_rounded']",
-            "//label[@id='dot']", "//label[@id='heart']", "//label[@id='diamond']"
-        ]
-        return self.select_random_option(pattern_locators)
+    # def select_pattern_step3(self):
+    #     self.locator.qrcode_patterns_step3_dropdown.click()
+    #     pattern_locators = [
+    #         "//label[@id='square']", "//label[@id='round']", "//label[@id='extra_rounded']",
+    #         "//label[@id='dot']", "//label[@id='heart']", "//label[@id='diamond']"
+    #     ]
+    #     return self.select_random_option(pattern_locators)
 
     def select_qrcode_corners_step3(self):
+        self.locator.qrcode_corners_step3_dropdown.is_enabled()
+        self.locator.qrcode_corners_step3_dropdown.scroll_into_view_if_needed(timeout=60000)
         self.locator.qrcode_corners_step3_dropdown.click()
         self.select_random_option([
             "//label[@id='NS']", "//label[@id='FR']", "//label[@id='FS']",
@@ -191,56 +174,60 @@ class QrCodeHelper:
             "//label[@id='IR']", "//label[@id='IDD']", "//label[@id='IF']", "//label[@id='IL']"
         ])
 
-    def select_random_social_network_option(self):
-        social_networks_locators = [
-            "//button[@id='socialicon_id_web']", "//button[@id='socialicon_id_dribbble']",
-            "//button[@id='socialicon_id_facebook']", "//button[@id='socialicon_id_flickr']",
-            "//button[@id='socialicon_id_gitHub']", "//button[@id='socialicon_id_GitLab']",
-            "//button[@id='socialicon_id_Google Review']", "//button[@id='socialicon_id_line']",
-            "//button[@id='socialicon_id_linkedIn']", "//button[@id='socialicon_id_pinterest']",
-            "//button[@id='socialicon_id_reddit']", "//button[@id='socialicon_id_skype']",
-            "//button[@id='socialicon_id_snapchat']", "//button[@id='socialicon_id_tripAdvisor']",
-            "//button[@id='socialicon_id_tumblr']", "//button[@id='socialicon_id_X']",
-            "//button[@id='socialicon_id_vimeo']", "//button[@id='socialicon_id_vkontakte']",
-            "//button[@id='socialicon_id_xing']", "//button[@id='socialicon_id_YouTube']",
-            "//button[@id='socialicon_id_instagram']", "//button[@id='socialicon_id_TikTok']",
-            "//button[@id='socialicon_id_WhatsApp']", "//button[@id='socialicon_id_telegram']",
-            "//button[@id='socialicon_id_Facebook Messenger']", "//button[@id='socialicon_id_yelp']",
-            "//button[@id='socialicon_id_Uber Eats']", "//button[@id='socialicon_id_postmates']",
-            "//button[@id='socialicon_id_OpenTable']", "//button[@id='socialicon_id_spotify']",
-            "//button[@id='socialicon_id_SoundCloud']", "//button[@id='socialicon_id_Apple Music']",
-            "//button[@id='socialicon_id_OnlyFans']", "//button[@id='socialicon_id_DoorDash']",
-            "//button[@id='socialicon_id_trustpilot']", "//button[@id='socialicon_id_signal']",
-            "//button[@id='socialicon_id_WeChat']"
-        ]
-        return self.select_random_option(social_networks_locators)
+    def add_phone_email_website(self):
+        self.locator.contact_details_qr_code_add_phone_btn.is_visible()
+        self.locator.contact_details_qr_code_add_phone_btn.is_enabled()
+        self.locator.contact_details_qr_code_add_phone_btn.click()
+        self.locator.contact_details_qr_code_add_phone_label.fill(self.faker.word())
+        self.locator.contact_details_qr_code_add_phone_number.fill(self.faker.basic_phone_number())
+        self.locator.contact_details_qr_code_add_email_btn.click()
+        self.locator.contact_details_qr_code_add_email_label.fill(self.faker.word())
+        self.locator.contact_details_qr_code_add_email_address.fill(self.faker.email())
+        self.locator.contact_details_qr_code_add_website_btn.click()
+        self.locator.contact_details_qr_code_add_website_label .fill(self.faker.word())
+        self.locator.contact_details_qr_code_add_website_url.fill(self.faker.url())
 
-    def set_screenshot_path(self, screenshot_path):
-        self.screenshot_path = screenshot_path
+    def set_location(self):
+        self.locator.location_qr_code_search_address.click()
+        self.locator.location_qr_code_search_address.fill(self.faker.city())
+        self.page.keyboard.press("ArrowDown")
+        self.page.keyboard.press("Enter")
+
+    def select_random_child_by_attribute(self, parent_selector, child_tag, unique_attribute):
+        parent_element = self.page.query_selector(parent_selector)
+        if not parent_element:
+            return
+        child_elements = parent_element.query_selector_all(f"{child_tag}[{unique_attribute}]")
+        if not child_elements:
+            return
+        attribute_values = [child.get_attribute(unique_attribute) for child in child_elements if
+                            child.get_attribute(unique_attribute)]
+        if not attribute_values:
+            return
+        random_value = random.choice(attribute_values)
+        for child in child_elements:
+            if child.get_attribute(unique_attribute) == random_value:
+                child.click(force=True)
+                break
 
     def take_iframe_screenshot(self):
         self.page.add_style_tag(content="""
-        .card {
-            border-radius: 0px !important;
-            width: 430px !important;
-        }
-        .mb-frame-inner .card {
-            max-width: none !important; 
-        }
-        .mb-frame-inner .card::after {
-            background-image: none !important;
-        }
-        #iframesrc, #iframesrc * {
-            border-radius: 0px !important;
-        }
+            .card {
+                border-radius: 0px !important;
+            }
+            .mb-frame-inner .card::after {
+                background-image: none !important;
+            }
+            #iframesrc, #iframesrc * {
+                border-radius: 0px !important;
+            }
         """)
-        footer_element = self.page.locator("//div[@id='qr-proceed-footer']")
-        footer_element.evaluate("element => element.style.display = 'none'")
         mobile = self.page.locator("//div[@id='tabs-1']/div")
         iphone_line = self.page.locator("//div[@class='iphone-line']")
         iphone_line.evaluate("element => element.style.position = 'none'")
         mobile.evaluate("element => element.style.height = '100vh'")
         mobile.evaluate("element => element.style.backgroundImage = 'none'")
         iframe = self.page.frame_locator("//iframe[@id='iframesrc']")
-        iframe.locator("//div[@class='App']").screenshot(path=str(self.screenshot_path))
-        footer_element.evaluate("element => element.style.display = ''")
+        timestamp = int(time.time())
+        self.screenshot_path = f"frame/test_{timestamp}.png"
+        iframe.locator("//div[@class='App']").screenshot(path=self.screenshot_path)
