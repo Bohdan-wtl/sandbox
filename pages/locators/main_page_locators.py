eaca04172e2dd17e966a454953a1ee7c6571306f
Bohdan-wtl <Bohdan-wtl@users.noreply.github.com>
2024-11-13 12:13:59 +0000

    Deploying to gh-pages from @ Bohdan-wtl/sandbox@fcaa149663e93871f92b4129e90047acbdaa573e 🚀

--- pages/locators/main_page_locators.py
+++
@@ -1,6 +0,0 @@
-class MainPageLocators:
-
-    def __init__(self, page):
-        self.log_in_button = page.get_by_role("link", name=" Log In")
-        self.sign_up_button = page.get_by_role("link", name=" Sign Up")
-        self.main_logo_link = page.locator(".billing-nav .animate-logo")
